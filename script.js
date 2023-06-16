e24d221a18c5cbdd17f58e75922e43e8b127aac6
hurstacy <107961442+hurstacy@users.noreply.github.com>
2023-06-16 17:01:42 -0400

    add icon

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Retrieve the count element
-    var countElement = document.getElementById('count');
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Retrieve the count element
+//     var countElement = document.getElementById('count');
     
-    // Set the initial count value to 508
-    var count = 508;
+//     // Set the initial count value to 508
+//     var count = 508;
   
-    // Get today's date
-    var today = new Date().toLocaleDateString();
+//     // Get today's date
+//     var today = new Date().toLocaleDateString();
   
-    // Get the stored date and count from local storage
-    var storedDate = localStorage.getItem('updateDate');
-    var storedCount = localStorage.getItem('count');
+//     // Get the stored date and count from local storage
+//     var storedDate = localStorage.getItem('updateDate');
+//     var storedCount = localStorage.getItem('count');
   
-    // If there is a stored date and it is today, update the count value
-    if (storedDate && storedDate === today) {
-      // Increment the stored count value by 1
-      count = parseInt(storedCount, 10) + 1;
-    }
+//     // If there is a stored date and it is today, update the count value
+//     if (storedDate && storedDate === today) {
+//       // Increment the stored count value by 1
+//       count = parseInt(storedCount, 10) + 1;
+//     }
   
-    // Update the count element with the new value
-    countElement.innerText = count;
+//     // Update the count element with the new value
+//     countElement.innerText = count;
   
-    // Store today's date and the updated count in local storage
-    localStorage.setItem('updateDate', today);
-    localStorage.setItem('count', count.toString());
-  });
+//     // Store today's date and the updated count in local storage
+//     localStorage.setItem('updateDate', today);
+//     localStorage.setItem('count', count.toString());
+//   });
   
